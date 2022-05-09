d031aac9bb0d47d0eaf9f13830ffdc7ff4c4151a
Pradyuman <pradyumangodhani006@gmail.com>
2022-05-09 15:59:53 +0530

    09-05-2022

--- app/src/main/java/com/example/quotesapp/Screen/Quotes_Screen/Controller/Quotes_Screen_Adapter.java
+++ app/src/main/java/com/example/quotesapp/Screen/Quotes_Screen/Controller/Quotes_Screen_Adapter.java
@@ -3,10 +3,18 @@ package com.example.quotesapp.Screen.Quotes_Screen.Controller;
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -21,6 +29,9 @@ import com.example.quotesapp.R;
 import com.example.quotesapp.Screen.Quotes_Screen.Model.Quotes_Screen_ModelData;
 import com.example.quotesapp.Screen.Quotes_Screen.View.quotes_screen;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -72,6 +83,7 @@ public class Quotes_Screen_Adapter extends RecyclerView.Adapter<Quotes_Screen_Ad
             }
         });
 
+
         holder.copy_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -88,7 +100,7 @@ public class Quotes_Screen_Adapter extends RecyclerView.Adapter<Quotes_Screen_Ad
 
                 Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
                 sharingIntent.setType("text/plain");
-                String shareBody = "Hello USER,\nPlease Rate Quotes App On Play Store\n⭐⭐⭐⭐⭐\n\nYOUR QUOTE\n \uD83D\uDC47\uD83D\uDC47\uD83D\uDC47\uD83D\uDC47\uD83D\uDC47\n\n"+Text;
+                String shareBody = "Hello USER,\nPlease Rate Quotes App On Play Store\n⭐⭐⭐⭐⭐\n\nYOUR QUOTE\n \uD83D\uDC47\uD83D\uDC47\uD83D\uDC47\uD83D\uDC47\uD83D\uDC47\n\n" + Text;
                 sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, Text);
                 sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
                 activity.startActivity(Intent.createChooser(sharingIntent, "Share via"));
@@ -100,7 +112,7 @@ public class Quotes_Screen_Adapter extends RecyclerView.Adapter<Quotes_Screen_Ad
             @Override
             public void onClick(View view) {
 
-               holder.like_img.setImageResource(R.drawable.red_like);
+                holder.like_img.setImageResource(R.drawable.red_like);
 
 
             }
@@ -110,7 +122,17 @@ public class Quotes_Screen_Adapter extends RecyclerView.Adapter<Quotes_Screen_Ad
             @Override
             public void onClick(View view) {
 
-                Toast.makeText(activity, "Coming Soon...", Toast.LENGTH_SHORT).show();
+                holder.img.setDrawingCacheEnabled(true);
+                Bitmap bitamp = holder.img.getDrawingCache();
+
+                try {
+                    saveBitmap(activity,bitamp, Bitmap.CompressFormat.PNG,"image/*","newimg.png");
+                }catch(Exception e) {
+
+                }
+
+
+
 
             }
         });
@@ -131,11 +153,54 @@ public class Quotes_Screen_Adapter extends RecyclerView.Adapter<Quotes_Screen_Ad
         clipboard.setPrimaryClip(clip);
     }
 
+    @NonNull
+    public Uri saveBitmap(@NonNull final Context context, @NonNull final Bitmap bitmap, @NonNull final Bitmap.CompressFormat format, @NonNull final String mimeType, @NonNull final String displayName) throws IOException {
+
+        final String relativeLocation = Environment.DIRECTORY_DCIM + File.separator + "Quotes App";
+        final ContentValues values = new ContentValues();
+        values.put(MediaStore.MediaColumns.DISPLAY_NAME, displayName);
+        values.put(MediaStore.MediaColumns.MIME_TYPE, mimeType);
+        values.put(MediaStore.MediaColumns.RELATIVE_PATH, relativeLocation);
+
+        final ContentResolver resolver = context.getContentResolver();
+        Uri uri = null;
+
+        try {
+            final Uri contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+            uri = resolver.insert(contentUri, values);
+
+            if (uri == null)
+                throw new IOException("Failed to create new MediaStore record.");
+
+            try (final OutputStream stream = resolver.openOutputStream(uri)) {
+                if (stream == null)
+                    throw new IOException("Failed to open output stream.");
+
+                if (!bitmap.compress(format, 95, stream))
+                    throw new IOException("Failed to save bitmap.");
+            }
+
+            Toast.makeText(context, "Image Saved To\n" + uri, Toast.LENGTH_SHORT).show();
+
+            return uri;
+        } catch (IOException e) {
+
+            if (uri != null) {
+                // Don't leave an orphan entry in the MediaStore
+                resolver.delete(uri, null, null);
+            }
+
+            throw e;
+        }
+    }
+
+
     public class ViewData extends RecyclerView.ViewHolder {
 
         TextView quotes_txt;
-        ImageView change_images_btn,like_img;
-        LinearLayout copy_btn,share_btn,like_btn,download_btn;
+        ImageView change_images_btn, like_img;
+        LinearLayout copy_btn, share_btn, like_btn, download_btn;
+        FrameLayout img;
 
         public ViewData(@NonNull View itemView) {
             super(itemView);
@@ -147,7 +212,12 @@ public class Quotes_Screen_Adapter extends RecyclerView.Adapter<Quotes_Screen_Ad
             share_btn = itemView.findViewById(R.id.share_btn);
             like_img = itemView.findViewById(R.id.like_img);
             download_btn = itemView.findViewById(R.id.download_btn);
+            img = itemView.findViewById(R.id.img);
 
         }
+
+
+
     }
 }
+
